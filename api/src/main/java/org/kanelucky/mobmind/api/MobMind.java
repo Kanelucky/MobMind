d010dd0064d35bbdbf8ac204e8d629820615c5e8
Kanelucky <tuankietdoan46@gmail.com>
2026-07-21 09:29:03 +0700

    deprecate(api): deprecate register() and init() in favor of init(MobMindInitializer)

--- api/src/main/java/org/kanelucky/mobmind/api/MobMind.java
+++ api/src/main/java/org/kanelucky/mobmind/api/MobMind.java
@@ -2,29 +2,32 @@ package org.kanelucky.mobmind.api;
 
 import org.kanelucky.mobmind.api.entity.MobMindInitializer;
 
-import java.util.ServiceLoader;
-
 public final class MobMind {
 
     public static final MobMind INSTANCE = new MobMind();
 
-    private MobMind() {}
-
-    public void init() {
-        if (initializer == null) throw new IllegalStateException(
-                "No initializer registered. Call MobMind.INSTANCE.register() first"
-        );
-        initializer.initialize();
+    private MobMind() {
     }
 
+    /**
+     * @deprecated Use {@link #init(MobMindInitializer)} instead
+     */
+    @Deprecated
     public void register(MobMindInitializer initializer) {
         this.initializer = initializer;
     }
 
+    /**
+     * @deprecated Use {@link #init(MobMindInitializer)} instead
+     */
+    @Deprecated
+    public void init() {
+        if (initializer == null) throw new IllegalStateException("No initializer registered. Call MobMind.INSTANCE.init(CoreInitializer) instead");
+        initializer.initialize();
+    }
+
     public void init(MobMindInitializer initializer) {
-        if (initializer == null) throw new IllegalStateException(
-                "Initializer cannot be null"
-        );
+        if (initializer == null) throw new IllegalStateException("Initializer cannot be null");
         initializer.initialize();
     }
 
